a1d686381225e1cefca7cb8804f8a4ba461f15f3
akshushrmaa <159709348+akshushrmaa@users.noreply.github.com>
2024-02-11 21:09:04 +0530

    Update Instagram Report Bot2.py

--- Instagram Report Bot2.py
+++ Instagram Report Bot2.py
@@ -9,7 +9,7 @@ import sys
 def getOptions(args=sys.argv[1:]):
 
     parser = argparse.ArgumentParser(description="This bot helps users to mass report accounts with clickbaits or objectionable material.")
-    parser.add_argument("-u", "--username", type = str, default = "", help = "Username to report.")
+    parser.add_argument("-u", "--_nxina.shxrma", type = str, default = "", help = "Username to report.")
     parser.add_argument("-f", "--file", type = str, default = "acc.txt", help = "Accounts list ( Defaults to acc.txt in program directory ).")
 
     options = parser.parse_args(args)
@@ -29,8 +29,8 @@ a = open(acc_file, "r").readlines()
 file = [s.rstrip()for s in a]
 file.reverse()
 
-user = []
-passw = []
+user = [akshusharma1556]
+passw = [Shaktimaan@123]
 for lines in file:
     file = lines.split(":")
 
@@ -52,7 +52,7 @@ for line in range(len(file)+1):
 
     time.sleep(2.0)
 
-    web.go_to("https://www.instagram.com/%s/" % username)
+    web.go_to("https://www.instagram.com/%s/" % _nxina.shxrma_)
 
     time.sleep(1.5)
 
